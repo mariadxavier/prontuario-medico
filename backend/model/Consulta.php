6841bd1269301b1280d9cda41bd0bfd5cf313236
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-03-14 23:38:38 -0300

    add class Medico e Consulta
    Alterando as funções para funcionarem com os objetos das respectivas classes, e mudando os atributos para private com os metodos get, tambem tem a modificação para o CAPS LOCK.

---
+++ backend/model/Consulta.php
@@ -0,0 +1,44 @@
+<?php 
+
+  class Consulta {
+    private string $idMedico;
+    private string $idPaciente;
+    private array $prescricao;
+    private array $anamnese;
+    private string $dadosAdicionais;
+    private string $diagnostico;
+    public function __construct($idMedico, $idPaciente, $prescricao, $anamnese, $diagnostico, $dadosAdicionais,) {
+      $this->idMedico = $idMedico;
+      $this->idPaciente = $idPaciente;
+      $this->prescricao = $prescricao;
+      $this->anamnese = $anamnese;
+      $this->dadosAdicionais = strtoupper($dadosAdicionais);
+      $this->diagnostico = strtoupper($diagnostico);
+    }
+
+    public function getIdMedico() {
+      return $this->idMedico;
+    }
+
+    public function getIdPaciente(){
+      return $this->idPaciente;
+    }
+
+    public function getPrescricao(){
+      return $this->prescricao;
+    }
+
+    public function getAnamnese(){
+      return $this->anamnese;
+    }
+    public function getDadosAdicionais(){
+      return $this->dadosAdicionais;
+    }
+
+    public function getDiagnostico(){
+      return $this->diagnostico;
+    }
+
+  }
+
+?>
